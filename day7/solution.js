4870b7b73aa0c252942b6d070d1da44b2e2abc66
Evan Sailer <esailer@360CloudSolutions.com>
2015-12-07 16:03:20 -0700

    updated day 7 solution

--- day7/solution.js
+++ day7/solution.js
@@ -2,16 +2,19 @@ var ehom = ehom || {};
 
 ehom.santa = ehom.santa || {};
 
-ehom.santa.LightGrid = function LightGrid() {
-    this.lights = Array(1000000);
-}
+ehom.santa.circuit = function circuit(input, w) {
 
-ehom.santa.circuit = function circuit(input) {
-
-    var instructions = input.split(':');
+    var instructions = [];
+    if (Array.isArray(input)) {
+        instructions = input;
+    } else {
+        instructions = input.split(':');
+    }
 
     var self = ehom.santa;
-    var wires = {};
+    var wires = w || {};
+    var in1, in2 = 0;
+    var retryInstructions = [];
 
     for (var i = 0; i < instructions.length; i++) {
         var ins = instructions[i].match(/([A-Z]+)/);
@@ -21,24 +24,71 @@ ehom.santa.circuit = function circuit(input) {
         var parts = instructions[i].split(' ');
 
         if (ins == 'AND') {
-            wires[parts[4]] = parseInt(parts[0], 10) & parseInt(parts[2], 10);
+            in1 = self.getInput(parts[0], wires);
+            in2 = self.getInput(parts[2], wires);
+            if (in1 == 'skip' || in2 == 'skip') {
+                retryInstructions.push(instructions[i]);
+                continue;
+            }
+            wires[parts[4]] = (in1 & in2) << 16 >>> 16;
         } else if (ins == 'LSHIFT') {
-            wires[parts[4]] = (wires[parts[0]] || 0) << parseInt(parts[2], 10);
+            in1 = self.getInput(parts[0], wires);
+            in2 = self.getInput(parts[2], wires);
+            if (in1 == 'skip' || in2 == 'skip') {
+                retryInstructions.push(instructions[i]);
+                continue;
+            }
+            wires[parts[4]] = (in1 << (in2 + 16) >>> 16);
         } else if (ins == 'NOT') {
-            wires[parts[3]] = ~(wires[parts[1]] || 0);
+            in1 = self.getInput(parts[1], wires);
+            if (in1 == 'skip') {
+                retryInstructions.push(instructions[i]);
+                continue;
+            }
+            wires[parts[3]] = (~in1 >>> 16) - self.getInput(parts[1], wires);
         } else if (ins == 'OR') {
-            wires[parts[4]] = parseInt(parts[0], 10) | parseInt(parts[2], 10);
+            in1 = self.getInput(parts[0], wires);
+            in2 = self.getInput(parts[2], wires);
+            if (in1 == 'skip' || in2 == 'skip') {
+                retryInstructions.push(instructions[i]);
+                continue;
+            }
+            wires[parts[4]] = (in1 | in2) << 16 >>> 16;
         } else if (ins == 'RSHIFT') {
-            wires[parts[4]] = (wires[parts[0]] || 0) >>> parseInt(parts[2], 10);
+            in1 = self.getInput(parts[0], wires);
+            in2 = self.getInput(parts[2], wires);
+            if (in1 == 'skip' || in2 == 'skip') {
+                retryInstructions.push(instructions[i]);
+                continue;
+            }
+            wires[parts[4]] = in1 >>> in2;
         } else {
             // assignment
-            if (Number.isInteger(parseInt(parts[0], 10))) {
-                wires[parts[2]] = parseInt(parts[0], 10);
-            } else {
-                wires[parts[2]] = wires[parts[0]] || 0;
+            in1 = self.getInput(parts[0], wires);
+            if (in1 == 'skip') {
+                retryInstructions.push(instructions[i]);
+                continue;
             }
+            wires[parts[2]] = in1;
         }
     }
+    if (retryInstructions.length) {
+        self.circuit(retryInstructions, wires);
+    }
     console.log(JSON.stringify(wires));
-    return wires['a'];
+    return wires.a;
+};
+
+
+ehom.santa.getInput = function getInput(x, wires) {
+    if (Number.isInteger(parseInt(x,10))) {
+        return parseInt(x,10);
+    }
+
+    // if wires[x]
+    if (wires[x] === undefined) {
+        return 'skip';
+    }
+
+    return wires[x];
 };
