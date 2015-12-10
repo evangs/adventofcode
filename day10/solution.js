479e51ad93eea621f81dfdd83b0e3e345ba7949a
Evan Sailer <esailer@360CloudSolutions.com>
2015-12-10 12:50:32 -0700

    even faster

--- day10/solution.js
+++ day10/solution.js
@@ -4,29 +4,42 @@ ehom.santa = ehom.santa || {};
 
 ehom.santa.lookAndSay = function lookAndSay(input) {
 
-    var newSequence = '';
+    var newSequence = new Int8Array(input.length * 2);
     var count = 0;
+    var index = 0;
 
     for (var i = 0; i < input.length; i++) {
         if (i > 0 && input[i-1] != input[i]) {
-            newSequence += count + input[i-1];
+            newSequence[index] = count;
+            index++;
+            newSequence[index] = input[i-1];
+            index++;
             count = 1;
         } else {
             count++;
         }
     }
 
-    newSequence += count + input[i-1];
+    newSequence[index] = count;
+    index++;
+    newSequence[index] = input[i-1];
 
-    return newSequence;
+    var slice = -1;
+    for (var p = input.length; p < newSequence.length; p++) {
+        if (newSequence[p] === 0) {
+            slice = p;
+            break;
+        }
+    }
+    return newSequence.slice(0, slice);
 };
 
 ehom.santa.nTimes = function nTimes(input, n) {
-
+    var start = new Date().getTime();
     for (i = 0; i < n; i++) {
         input = ehom.santa.lookAndSay(input);
     }
-
+    console.log('execution time: ' + (new Date().getTime() - start) / 1000);
     return input.length;
 };
 
@@ -38,7 +51,7 @@ ehom.santa.oneMin = function oneMin(input) {
 
         input = ehom.santa.lookAndSay(input);
         iterations++;
-        console.log(iterations);
+        console.log(iterations + ' ' + currentTime);
     }
 
     return {'executionTime': currentTime, 'iterations': iterations, 'length': input.length};
