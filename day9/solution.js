792923c570169756a1910e2d9a69f9ff03902431
Evan Sailer <esailer@asu.edu>
2015-12-09 21:14:49 -0700

    updated with final solution

--- day9/solution.js
+++ day9/solution.js
@@ -5,7 +5,7 @@ ehom.santa = ehom.santa || {};
 ehom.santa.Node = function Node(name) {
     this.name = name;
     this.tentative = 0;
-    this.destinations = [];
+    this.destinations = {};
 };
 
 ehom.santa.Dest = function Dest(name, dist) {
@@ -32,121 +32,78 @@ ehom.santa.shortestPath = function shortestPath(input) {
         var parts = paths[i].split(' ');
         if (!shortMap[parts[0]]) {
             shortMap[parts[0]] = new self.Node(parts[0]);
-            shortMap[parts[0]].destinations.push(new self.Dest(parts[2], parseFloat(parts[4])));
+            shortMap[parts[0]].destinations[parts[2]] = new self.Dest(parts[2], parseFloat(parts[4]));
         } else {
 
             n = new self.Dest(parts[2], parseFloat(parts[4]));
-            if (shortMap[parts[0]].destinations.indexOf(n) == -1) {
-                shortMap[parts[0]].destinations.push(new self.Dest(parts[2], parseFloat(parts[4])));
+            if (!shortMap[parts[0]].destinations[parts[2]]) {
+                shortMap[parts[0]].destinations[parts[2]] = new self.Dest(parts[2], parseFloat(parts[4]));
             }
         }
 
         if (!shortMap[parts[2]]) {
             shortMap[parts[2]] = new self.Node(parts[2]);
-            shortMap[parts[2]].destinations.push(new self.Dest(parts[0], parseFloat(parts[4])));
+            shortMap[parts[2]].destinations[parts[0]] = new self.Dest(parts[0], parseFloat(parts[4]));
         } else {
 
             n = new self.Dest(parts[0], parseFloat(parts[4]));
-            if (shortMap[parts[2]].destinations.indexOf(n) == -1) {
-                shortMap[parts[2]].destinations.push(new self.Dest(parts[0], parseFloat(parts[4])));
+            if (!shortMap[parts[2]].destinations[parts[0]]) {
+                shortMap[parts[2]].destinations[parts[0]] = new self.Dest(parts[0], parseFloat(parts[4]));
             }
         }
     }
 
+    var locations = [];
     for (entry in shortMap) {
-        shortMap[entry].destinations.sort(function(a,b){
-            return a.distance-b.distance;
-        });
-        console.log(entry);
-        console.log(shortMap[entry].destinations);
-        console.log('\n');
+        locations.push(entry);
     }
 
-//    return shortPath;
-};
+    var permutations = ehom.santa.permutator(locations);
+    console.log(permutations.length);
 
-ehom.santa.visitNode = function visitNode(currentNode, unvisitedNodes, shortMap) {
-    for (var x = 0; x < shortMap[currentNode].destinations.length; x++) {
-        if (unvisitedNodes.indexOf(shortMap[currentNode].destinations[x].name) > -1) {
-            if ((shortMap[currentNode].destinations[x].distance + shortMap[currentNode].tentative) < shortMap[shortMap[currentNode].destinations[x].name].tentative) {
-                shortMap[shortMap[currentNode].destinations[x].name].tentative = (shortMap[currentNode].destinations[x].distance + shortMap[currentNode].tentative);
-            }
+    var longest = 0;
+    var shortest = Infinity;
+    for (var w = 0; w < permutations.length; w++) {
+        var pathd = 0;
+        for (var a = 0; a < permutations[w].length - 1; a++) {
+            pathd += shortMap[permutations[w][a]].destinations[permutations[w][a + 1]].distance;
+        }
+
+        if (pathd > longest) {
+            longest = pathd;
+        }
+
+        if (pathd < shortest) {
+            shortest = pathd;
         }
     }
+
+    console.log(shortest);
+    console.log(longest);
+};
+
+ehom.santa.permutator = function permutator(inputArr) {
+  var results = [];
+
+  function permute(arr, memo) {
+    var cur, memo = memo || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
 };
 
 
-/*
-8
-alphacentauri
-faerun 3
-tristram 65
-norrath 4
-snowdin 8
-tambi 52
-straylight 65
-arbre 14
-*/
 
 
-/*
-arbre
-faerun 149
-norrath 144
-alphacentauri 136
-straylight 107
-tristram 125
-snowdin 105
-tambi 52
-818 */
 
-/*
-faerun
-norrath 144
-alphacentauri 136
-straylight 107
-tristram 125
-snowdin 105
-arbre 96
-tambi 143
-856 */
-
-/*
-faerun
-straylight 137
-tristram 125
-snowdin 105
-arbre 96
-tambi 143
-norrath 68
-alphacentauri 136
-810 */
-
-/*
-snowdin
-tristram 105
-straylight 125
-faerun 137
-tambi 129
-arbre 143
-norrath 115
-alphacentauri 136
-890 */
-
-/*
-alphacentauri
-norrath 136
-faerun 144
-straylight 137
-tristram 125
-snowdin 105
-arbre 96
-tambi 143
-886 */
-
-
-/*
-snowdin
-straylight 101
-tristram 125
-*/
